6564f9342f0c8c40654565b155ef3e47d5092903
ohampton <44102431+ohampton@users.noreply.github.com>
2022-02-16 18:54:26 -0600

    skillsSectionCommit1

---
+++ main.js
@@ -0,0 +1,22 @@
+// Nav bar effects on scroll
+    window.addEventListener("scroll" , function() {
+        const header = document.querySelector("header")
+        header.classList.toggle("sticky", window.scrollY > 0 )
+    })
+// Services Section 
+
+// Portfolio Section
+
+// Our Clients - Swiper
+
+// Dark Light Theme
+
+// Scroll To Top Button
+
+// Nav Menu Items On Page Scroll 
+
+// Nav Menu Toggle 
+
+// Responsive Nav Menu Toggle
+
+// scroll reveal animations 
