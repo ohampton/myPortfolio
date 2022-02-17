b012c12cf55da7a5fac529e8add88cf7141d4926
ohampton <44102431+ohampton@users.noreply.github.com>
2022-02-17 15:05:29 -0600

    portfolioCommit1

--- main.js
+++ main.js
@@ -26,7 +26,28 @@
         });
     });
 
-// Portfolio Section
+// Portfolio Section Modal
+    const portfolioModals = document.querySelectorAll(".portfolio-model")
+    const imgCards = document.querySelectorAll(".img-card")
+    const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn")
+
+    let portfolioModal = function(modalClick){
+        portfolioModals[modalClick].classList.add("active");
+    }
+
+    imgCards.forEach((imgCard, i) => {
+        imgCard.addEventListener("click", () => {
+            portfolioModal(i);
+        });
+    });
+
+    portfolioCloseBtns.forEach((portfolioCloseBtns) => {
+        portfolioCloseBtns.addEventListener("click", () => {
+            portfolioModals.forEach((portfolioModalView) => {
+                portfolioModalView.classList.remove('active');
+            });
+        });
+    });
 
 // Our Clients - Swiper
 
