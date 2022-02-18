208a77381479e72d33b991a0039ac73c3796300f
ohampton <44102431+ohampton@users.noreply.github.com>
2022-02-18 13:54:52 -0600

    scrollCommitFinal

--- main.js
+++ main.js
@@ -128,8 +128,21 @@ navItems.forEach((navItem) => {
 });
 // scroll reveal animations 
 // common reveal options to create reveal animations
+ScrollReveal({
+    reset: true, 
+    distance: '60px',
+    duration: 2500,
+    delay:100
+});
 
-
-
-
-// Nav Menu Toggle 
+// Reveal Animations 
+ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay: 500, origin: 'left'});
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn',  { delay: 600, origin: 'right'});
+ScrollReveal().reveal('.home .info .btn', { delay: 700, origin: 'bottom'});
+ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200});
+ScrollReveal().reveal('.home-img, .about-img', { delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.about .description, .contact-right', { delay: 500, origin: 'right'});
+ScrollReveal().reveal('.about .professional-list li', { delay: 500, origin: 'right', interval:200});
+ScrollReveal().reveal('.skills-description, .skills-info, .services-description, .contact-card, .contact-left h2', { delay: 700, origin: 'left'});
+ScrollReveal().reveal('.experience-card, .service-card, .education .portfolio, .portfolio .img-card' , { delay: 800, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.footer .group' , { delay: 500, origin: 'top', interval: 200});
